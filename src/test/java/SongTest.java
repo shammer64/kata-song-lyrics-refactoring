4fff02b299e1cf0f849466e6ab5d0a72acc05a0a
Scott Hammer <shammer64@gmail.com>
2022-09-13 15:20:20 -0400

    Added approval test

--- src/test/java/SongTest.java
+++ src/test/java/SongTest.java
@@ -1,5 +1,24 @@
+import org.approvaltests.Approvals;
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.intellij.IntelliJCommunityReporter;
+import org.approvaltests.reporters.intellij.IntelliJReporter;
+import org.approvaltests.reporters.macosx.MacDiffReporter;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class SongTest {
 
-    // We don't need no stinking tests!
+    @Test
+    @UseReporter(IntelliJReporter.class)
+    public void approvalTest() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream((baos)));
+
+        Song.main(new String[]{});
+
+        Approvals.verify(baos.toString());
+    }
 
 }
