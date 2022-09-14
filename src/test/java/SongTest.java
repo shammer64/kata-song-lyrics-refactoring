d0f0e75ad2a7b98bec2df6263f6cbad1df65be97
Scott Hammer <shammer64@gmail.com>
2022-09-13 21:55:46 -0400

    Revert "Checkin approval tests"
    This reverts commit d0102b0aeabc5d1c74c27b9f4d95440b7d18aa95.

--- src/test/java/SongTest.java
+++ src/test/java/SongTest.java
@@ -1,22 +1,5 @@
-import org.approvaltests.Approvals;
-import org.approvaltests.reporters.UseReporter;
-import org.approvaltests.reporters.intellij.IntelliJReporter;
-import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 public class SongTest {
 
-    @Test
-    @UseReporter(IntelliJReporter.class)
-    public void approvalTest() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(baos));
-
-        Song.main(new String[]{});
-
-        Approvals.verify(baos.toString());
-    }
+    // We don't need no stinking tests!
 
 }
