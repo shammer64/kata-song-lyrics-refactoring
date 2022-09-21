16ae6a866a969d220a68a47f16fe70e5fec8c362
Scott Hammer <shammer64@gmail.com>
2022-09-20 20:30:52 -0400

    SH Finished second verse, started third verse

--- src/main/java/SongGenerator.java
+++ src/main/java/SongGenerator.java
@@ -5,71 +5,85 @@ public class SongGenerator {
 
         sb.append(firstStanza());
         sb.append(secondStanza());
-        String song = "There was an old lady who swallowed a bird;\n" +
-                        "How absurd to swallow a bird.\n" +
-                        "She swallowed the bird to catch the spider,\n" +
-                        "She swallowed the spider to catch the fly;\n" +
-                        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                        "\n" +
-                        "There was an old lady who swallowed a cat;\n" +
-                        "Fancy that to swallow a cat!\n" +
-                        "She swallowed the cat to catch the bird,\n" +
-                        "She swallowed the bird to catch the spider,\n" +
-                        "She swallowed the spider to catch the fly;\n" +
-                        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                        "\n" +
-                        "There was an old lady who swallowed a dog;\n" +
-                        "What a hog, to swallow a dog!\n" +
-                        "She swallowed the dog to catch the cat,\n" +
-                        "She swallowed the cat to catch the bird,\n" +
-                        "She swallowed the bird to catch the spider,\n" +
-                        "She swallowed the spider to catch the fly;\n" +
-                        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                        "\n" +
-                        "There was an old lady who swallowed a cow;\n" +
-                        "I don't know how she swallowed a cow!\n" +
-                        "She swallowed the cow to catch the dog,\n" +
-                        "She swallowed the dog to catch the cat,\n" +
-                        "She swallowed the cat to catch the bird,\n" +
-                        "She swallowed the bird to catch the spider,\n" +
-                        "She swallowed the spider to catch the fly;\n" +
-                        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                        "\n";
+        sb.append(thirdStanza());
+        String song = "There was an old lady who swallowed a cat;\n" +
+                "Fancy that to swallow a cat!\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a dog;\n" +
+                "What a hog, to swallow a dog!\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a cow;\n" +
+                "I don't know how she swallowed a cow!\n" +
+                "She swallowed the cow to catch the dog,\n" +
+                "She swallowed the dog to catch the cat,\n" +
+                "She swallowed the cat to catch the bird,\n" +
+                "She swallowed the bird to catch the spider,\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n";
         sb.append(song);
         sb.append(lastStanza());
 
         return sb.toString();
     }
 
+
+    private String firstStanza() {
+        return firstLineOfStanza("fly", ".") +
+                lastLineOfStanza();
+    }
+
     private String secondStanza() {
-        return firstLine("spider", ";") +
-                "That wriggled and wiggled and tickled inside her.\n" +
+        return firstLineOfStanza("spider", ";") +
+                secondLineOfStanza(2) +
+                thirdLineOfStanza() +
+                lastLineOfStanza();
+    }
+
+    private String thirdStanza() {
+        return firstLineOfStanza("bird", ";") +
+                secondLineOfStanza(3) +
+                "She swallowed the bird to catch the spider,\n" +
                 "She swallowed the spider to catch the fly;\n" +
-                secondLine();
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n";
     }
-
-    private String firstLineSecondStanza() {
-        return "There was an old lady who swallowed a spider;\n";
-    }
-
     private String lastStanza() {
-        return firstLine("horse", "...") +
+        return firstLineOfStanza("horse", "...") +
                 lastLineLastStanza();
+    }
+
+    private String firstLineOfStanza(String animal, String punc){
+        return String.format("There was an old lady who swallowed a %s%s\n", animal, punc);
+    }
+
+    private String secondLineOfStanza(int stanzaNumber) {
+        String[] secondLines = new String[] {
+                "That wriggled and wiggled and tickled inside her.\n",
+                "How absurd to swallow a bird.\n"
+        };
+        return secondLines[stanzaNumber - 2];
+    }
+
+    private String thirdLineOfStanza() {
+        return "She swallowed the spider to catch the fly;\n";
+    }
+
+    private String lastLineOfStanza() {
+        return "I don't know why she swallowed a fly - perhaps she'll die!\n\n";
     }
 
     private String lastLineLastStanza() {
         return "...She's dead, of course!";
     }
 
-    private String firstStanza() {
-        return firstLine("fly", ".") +
-                secondLine();
-    }
-
-    private String secondLine() {
-        return "I don't know why she swallowed a fly - perhaps she'll die!\n\n";
-    }
-    private String firstLine(String animal, String punc){
-        return String.format("There was an old lady who swallowed a %s%s\n", animal, punc);
-    }
 }
