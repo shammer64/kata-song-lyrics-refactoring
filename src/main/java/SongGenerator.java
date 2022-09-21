8fbefdf2cfbc0b17f1a3dd24ce433d3ef47a6782
Scott Hammer <shammer64@gmail.com>
2022-09-20 21:27:46 -0400

    SH Refactored to loops

--- src/main/java/SongGenerator.java
+++ src/main/java/SongGenerator.java
@@ -1,90 +1,61 @@
 public class SongGenerator {
 
+    public static final String[] SECOND_LINES = new String[]{
+            "That wriggled and wiggled and tickled inside her.\n",
+            "How absurd to swallow a bird.\n",
+            "Fancy that to swallow a cat!\n",
+            "What a hog, to swallow a dog!\n",
+            "I don't know how she swallowed a cow!\n"
+    };
+    public static final String[] ANIMALS = new String[]{
+            "fly", "spider", "bird", "cat", "dog", "cow", "horse"
+    };
+
     String generateLyrics() {
         StringBuffer sb = new StringBuffer();
 
         sb.append(firstStanza());
-        sb.append(secondStanza());
-        sb.append(thirdStanza());
-        sb.append(fourthStanza());
-        sb.append(fifthStanza());
-        sb.append(sixthStanza());
+        for (int stanzaNumber = 2; stanzaNumber <= 6; stanzaNumber++) {
+            sb.append(middleStanza(stanzaNumber));
+        }
         sb.append(lastStanza());
 
         return sb.toString();
     }
 
     private String firstStanza() {
-        return firstLineOfStanza("fly", ".") +
+        return firstLineOfStanza(1, ".") +
                 lastLineOfStanza();
     }
 
-    private String secondStanza() {
-        return firstLineOfStanza("spider", ";") +
-                secondLineOfStanza(2) +
-                middleLinesOfStanza(2) +
-                lastLineOfStanza();
-    }
-
-    private String thirdStanza() {
-        return firstLineOfStanza("bird", ";") +
-                secondLineOfStanza(3) +
-                middleLinesOfStanza(3) +
-                lastLineOfStanza();
-    }
-
-    private String fourthStanza() {
-        return firstLineOfStanza("cat", ";") +
-                secondLineOfStanza(4) +
-                middleLinesOfStanza(4) +
-                lastLineOfStanza();
-    }
-
-    private String fifthStanza() {
-        return firstLineOfStanza("dog", ";") +
-                secondLineOfStanza(5) +
-                middleLinesOfStanza(5) +
-                lastLineOfStanza();
-    }
-
-    private String sixthStanza() {
-        return firstLineOfStanza("cow", ";") +
-                secondLineOfStanza(6) +
-                middleLinesOfStanza(6) +
+    private String middleStanza(int stanzaNumber) {
+        return firstLineOfStanza(stanzaNumber, ";") +
+                secondLineOfStanza(stanzaNumber) +
+                middleLinesOfStanza(stanzaNumber) +
                 lastLineOfStanza();
     }
 
     private String lastStanza() {
-        return firstLineOfStanza("horse", "...") +
+        return firstLineOfStanza(7, "...") +
                 lastLineLastStanza();
     }
 
-    private String firstLineOfStanza(String animal, String punc){
-        return String.format("There was an old lady who swallowed a %s%s\n", animal, punc);
+    private String firstLineOfStanza(int stanzaNumber, String punc){
+        return String.format("There was an old lady who swallowed a %s%s\n", ANIMALS[stanzaNumber - 1], punc);
     }
 
     private String secondLineOfStanza(int stanzaNumber) {
-        String[] secondLines = new String[] {
-                "That wriggled and wiggled and tickled inside her.\n",
-                "How absurd to swallow a bird.\n",
-                "Fancy that to swallow a cat!\n",
-                "What a hog, to swallow a dog!\n",
-                "I don't know how she swallowed a cow!\n"
-        };
-        return secondLines[stanzaNumber - 2];
+        return SECOND_LINES[stanzaNumber - 2];
     }
 
     private String middleLinesOfStanza(int stanzaNumber) {
-        String[] animals = new String[] {
-                "fly", "spider", "bird", "cat", "dog", "cow"
-        };
         StringBuffer middleLines = new StringBuffer();
         for (int currentStanza = stanzaNumber; currentStanza >= 2; currentStanza--) {
             String punc = currentStanza == 2 ? ";" : ",";
             middleLines.append(
                     String.format("She swallowed the %s to catch the %s%s\n",
-                            animals[currentStanza - 1],
-                            animals[currentStanza - 2],
+                            ANIMALS[currentStanza - 1],
+                            ANIMALS[currentStanza - 2],
                             punc));
         }
         return middleLines.toString();
